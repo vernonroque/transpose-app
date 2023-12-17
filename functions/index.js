914a35827d842d6b1f37cf1c1f89a7e60bcc7e41
vernonroque <vroque88@gmail.com>
2023-12-17 13:16:18 -0500

    Added api routes

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,22 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+// API
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// API routes
+app.get("/", (request, response) => response.status(200).send("hey baus!"));
+
+// Listen command
+exports.api = functions.https.onRequest(app);
+
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -7,9 +26,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+/*
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
@@ -17,3 +36,4 @@ exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+*/
